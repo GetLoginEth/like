95226ad52904f93fa4d2b283b26d4ffb01595e79
Igor Shadurin <igor.shadurin@gmail.com>
2020-05-14 17:03:43 +0300

    connected modules

--- web/LikeInjector.js
+++ web/LikeInjector.js
@@ -5,30 +5,39 @@ class LikeInjector {
         this.getLoginInstance = getLoginInstance;
         this.contractAddress = '0x...';
         this.abi = [];
-        this.getLoginInstance.setClientAbi(this.abi);
+        /*this.getLoginInstance.setClientAbi(this.abi);
         this.getLoginInstance.setOnLogout(_ => {
             console.log('logout');
-        });
+        });*/
     }
 
     init() {
-        if (window && window.onLikeInjectorLoaded) {
-            window.onLikeInjectorLoaded(this);
+        // todo inject getlogin
+        if (window && window._onLikeInjectorLoaded) {
+            window._onLikeInjectorLoaded(this);
+            delete window._onLikeInjectorLoaded;
         }
+
+        window.addEventListener("message", (e) => {
+            // todo check is other apps can send here messages
+            const event = e.data.event;
+            const data = e.data.data;
+            console.log('received message', event, data);
+        }, false);
     }
 
-    draw(selector, likeUrl = null, resourceType = null, resourceId = null) {
+    draw(params) {
         const modes = {url: 'url', resource: 'resource'};
         let mode;
-        if (likeUrl) {
+        if (params.likeUrl) {
             mode = modes.url;
-        } else if (resourceType && resourceId) {
+        } else if (params.resourceType && params.resourceId) {
             mode = modes.resource;
         } else {
             throw new Error('Incorrect params')
         }
 
-        const element = document.querySelector(selector);
+        const element = document.querySelector(params.selector);
         if (!element) {
             throw new Error('Element not found');
         }
@@ -41,11 +50,7 @@ class LikeInjector {
 
         const iframe = document.createElement('iframe');
         iframe.setAttribute('style', 'border: 0');
-        iframe.src = './module.html';
-        /*iframe._likeModuleInstance = () => {
-            console.log('yep, _likeModuleInstance');
-        }*/
-
+        iframe.src = `./module.html?mode=${mode}&resourceType=${params.resourceType ? params.resourceType : ''}&resourceId=${params.resourceId ? params.resourceId : ''}`;
         element.appendChild(iframe);
     }
 }
--- web/LikeModule.js
+++ web/LikeModule.js
@@ -2,18 +2,21 @@
 
 class LikeModule {
     constructor(getLoginInstance) {
-        this.getLoginInstance = getLoginInstance;
+        /*this.getLoginInstance = getLoginInstance;
         this.contractAddress = '0x...';
         this.abi = [];
-        this.getLoginInstance.setClientAbi(this.abi);
+        /*this.getLoginInstance.setClientAbi(this.abi);
         this.getLoginInstance.setOnLogout(_ => {
             console.log('logout');
-        });
+        });*/
+        this.urlParams = new URLSearchParams(window.location.search);
+        // todo check is enough data in urlParams
     }
 
     init() {
-        if (window && window.onLikeModuleLoaded) {
-            window.onLikeModuleLoaded(this);
+        if (window && window._onLikeModuleLoaded) {
+            window._onLikeModuleLoaded(this);
+            delete window._onLikeModuleLoaded;
         }
     }
 
@@ -21,18 +24,18 @@ class LikeModule {
         alert('Click')
     }
 
-    draw(selector, likeUrl = null, resourceType = null, resourceId = null) {
+    /*draw(params) {
         const modes = {url: 'url', resource: 'resource'};
         let mode;
-        if (likeUrl) {
+        if (params.likeUrl) {
             mode = modes.url;
-        } else if (resourceType && resourceId) {
+        } else if (params.resourceType && params.resourceId) {
             mode = modes.resource;
         } else {
             throw new Error('Incorrect params')
         }
 
-        const element = document.querySelector(selector);
+        const element = document.querySelector(params.selector);
         if (!element) {
             throw new Error('Element not found');
         }
@@ -43,20 +46,20 @@ class LikeModule {
             // todo set action to like contentType + contentId hash
         }
 
-        const iframe = document.createElement('iframe');
-        iframe.setAttribute('style', 'border: 0');
-        iframe.src = './module.html';
-        /*iframe._likeModuleInstance = () => {
-            console.log('yep, _likeModuleInstance');
-        }*/
+        // todo check and inject getlogin iframe. One iframe per page
 
-        element.appendChild(iframe);
-    }
+        const likeIframe = document.createElement('iframe');
+        likeIframe.setAttribute('style', 'border: 0');
+        likeIframe.src = './module.html';
+        element.appendChild(likeIframe);
+    }*/
 }
 
 // todo load getlogin plugin, then load like module
 (new LikeModule({
-    test: 123, setClientAbi: () => {
-    }, setOnLogout: () => {
-    }
+    /*test: 123,
+    setClientAbi: () => {
+    },
+    setOnLogout: () => {
+    }*/
 })).init();
