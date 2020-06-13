c220a7ebe67484d73b47dd081adb0b263708d369
Igor Shadurin <igor.shadurin@gmail.com>
2020-06-13 16:47:43 +0300

    implemented auto-deploy

--- smart-deploy/deploy.js
+++ smart-deploy/deploy.js
@@ -1,10 +1,80 @@
 const fs = require('fs');
+const path = require('path');
+const solc = require('solc');
 const Web3 = require('web3');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
-const web3 = new Web3();
+async function start() {
+    console.log('Is only update logic: ', settings.isUpdateLogicOnly);
+    const likeStoragePath = path.resolve(__dirname, '../', 'smart', 'contracts', 'LikeStorage.sol');
+    const likeLogicPath = path.resolve(__dirname, '../', 'smart', 'contracts', 'LikeLogic.sol');
+    const sourceLikeStorage = fs.readFileSync(likeStoragePath, 'utf8');
+    const sourceLikeLogic = fs.readFileSync(likeLogicPath, 'utf8');
+    const input = {
+        language: 'Solidity',
+        sources: {
+            'LikeStorage.sol': {
+                content: sourceLikeStorage
+            },
+            'LikeLogic.sol': {
+                content: sourceLikeLogic
+            },
+        },
+        settings: {
+            outputSelection: {
+                '*': {
+                    '*': ['*']
+                }
+            }
+        }
+    };
+    let data = solc.compile(JSON.stringify(input));
+    //console.log(data);
+    fs.writeFileSync(path.resolve(__dirname, 'out.json'), data);
+
+    data = JSON.parse(data);
+    const bytecodeStorage = data.contracts["LikeStorage.sol"]["LikeStorage"].evm.bytecode.object;
+    const abiStorage = data.contracts["LikeStorage.sol"]["LikeStorage"].abi;
+    const bytecodeLogic = data.contracts["LikeLogic.sol"]["LikeLogic"].evm.bytecode.object;
+    const abiLogic = data.contracts["LikeLogic.sol"]["LikeLogic"].abi;
+
+    //const web3 = new Web3(new Web3.providers.WebsocketProvider(settings.wsprovider));
+    const web3 = new Web3(new HDWalletProvider(settings.privateKey, settings.httpsprovider));
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+    let result = null;
+    // todo set correct address after deploy (here only default address)
+    let storageContract = new web3.eth.Contract(abiStorage, settings.likeStorageAddress);
+    if (!settings.isUpdateLogicOnly) {
+        console.log('Publish storage');
+        // todo get gas before deploy
+        result = await storageContract
+            .deploy({data: '0x' + bytecodeStorage})
+            .send({/*gas: '3000000',*/ from: accounts[0]});
+        storageContract.options.address = result.options.address;
+        console.log('Storage contract deployed to', result.options.address);
+    }
+
+    const likeStorageParamAddress = settings.isUpdateLogicOnly ? settings.likeStorageAddress : result.options.address;
+    console.log('Publish logic with storage address', likeStorageParamAddress);
+    result = await new web3.eth.Contract(abiLogic)
+        .deploy({
+            data: '0x' + bytecodeLogic,
+            arguments: [likeStorageParamAddress, settings.getLoginStorageAddress]
+        })
+        .send({/*gas: '3000000',*/ from: accounts[0]});
+    console.log('Logic contract deployed to', result.options.address);
+    console.log('Set logic address to storage contract');
+    await storageContract.methods.setLogicAddress(result.options.address).send({from: accounts[0]});
+    console.log('Complete!');
+}
 
 const settings = JSON.parse(fs.readFileSync('../.rinkeby.json').toString());
-console.log(settings);
-// todo deploy like storage
-// todo deploy like logic with storage address and getLoginStorage address
-// todo log logic and storage addresses for like
+start()
+    .then(_ => {
+        process.exit();
+    })
+    .catch(e => {
+        console.log(e);
+        process.exit();
+    });
--- smart/truffle-config.js
+++ smart/truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+//const HDWalletProvider = require('@truffle/hdwallet-provider');
+/*const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKeyRinkeby = "357ce0ddb3ef426ba0bc727a3c64c873";
+const fs = require('fs');
+//const mnemonic = fs.readFileSync(".rinkeby").toString().trim();
+const privateKey = fs.readFileSync(".rinkeby").toString().trim();*/
 
 module.exports = {
     /**
@@ -60,14 +61,23 @@ module.exports = {
 
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        /*ropsten: {
+            provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
+            network_id: 3,       // Ropsten's id
+            gas: 5500000,        // Ropsten has a lower block limit than mainnet
+            confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },*/
+        /*rinkeby: {
+            provider: () => new HDWalletProvider(privateKey, `https://rinkeby.infura.io/v3/${infuraKeyRinkeby}`),
+            network_id: 4,       // Ropsten's id
+            gas: 4500000,        // Ropsten has a lower block limit than mainnet
+            gasPrice: 10000000000,        // Ropsten has a lower block limit than mainnet
+            //confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+            //timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+            //skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+        },*/
 
         // Useful for private networks
         // private: {
